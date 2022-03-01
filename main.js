45e17023f5d61bc72879cd0cfb155194871363f9
Jill Kidman <jillkidman@Jills-MacBook-Air.local>
2022-03-01 12:41:54 -0500

    intial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const prompt = require("prompt-sync")();
+
+while(){
+//display some test content to the user, and prompt for an action
+console.log("test content!");
+let catchOrRelease = prompt("action");
+
+
+}
+
+// code to generate random fish (WHICH WILL GO IN AN OBJECT!!!)
+//      == will need a randomly generated name WITH two adjectives and a type of fish. 
+//      == will also need a random weight and a random dollar value
+
+//generating a RANDOM FISH NAME-
+    //  create 3 arrays with 10 values in each:
+    //  1- ten fish adjectives
+    //  2- ten fish adjectives
+    //  3- ten fish types
+    
+
+
+
+//to generate the random name, take a RANDOM adjective from the first array, a RANDOM adjective from the second array, and a RANDOM fish type from the third array, and put them together
+    //to get a random value from an array, generate a random value from 0 and the last index in the array (array.length-1)
+
+
+// generate the random fish weight and value
+    // random weight between 0 and 10 with two decimal points
+    // random value between 0 and 1,000 then divide by 100, output in dollars/cents
